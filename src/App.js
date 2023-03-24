d80d06b4b7759983b8fbf3c42d7490af922fe9f0
ctrlpdf <ctrlpdf@gmail.com>
2023-03-24 16:04:10 +0900

    Sample charts added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import SampleAmcharts from './SampleAmcharts';
+import SampleEcharts from './SampleEcharts';
+import SamplePlotly from './SamplePlotly';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <SampleAmcharts chartID="amchrts1" />
+      </div>
+      <div>
+        <SampleEcharts />
+      </div>
+      <div>
+        <SamplePlotly />
+      </div>
     </div>
   );
 }
